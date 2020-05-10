cc9f01d2fe93cd2fd7544b6f6b84fbe72a734aa5
Enzo Esperguin <enzoesper@hotmail.com>
2020-05-10 14:45:56 -0300

    resolving with hardcoded data

--- schema/schema.js
+++ schema/schema.js
@@ -2,8 +2,15 @@ const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+const _ = require('lodash');
+
+const users = [
+  {id: '23', firstName: 'Bill', age: 20},
+  {id: '47', firstName: 'Jane', age: 35}
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -21,8 +28,12 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        
+        return _.find(users, { id: args.id });
       }
     }
   }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
